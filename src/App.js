c3725a48ab2057eb2bc61b4d1f53500e89a7b8c1
김선빈 <ksb9242@gmail.com>
2021-05-11 23:31:32 +0900

    react study 2

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@
 
 import './App.css';
 import { Navbar, Nav, NavDropdown, Button, Jumbotron } from 'react-bootstrap';
-import { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Data from './data.js';
 import Detail from './Detail.js';
+import axios from 'axios';
 
 // Route를 만드러보자 (페이지를 나누자)
 // yarn add react-router-dom
 import { Link, Route, Switch } from 'react-router-dom';
+
+// context 만들기
+// 1. React.createContext()로 범위생성
+// 2. 같은 값을 공유할 HTML을 범위로 싸매기
+// 3. value={ 공유원하는값 }
+let 재고context = React.createContext();
 
 // yarn add styled-components
 
 function App() {
 
   let [shoes, shoes변경] = useState(Data);
+  let [loding, loding변경] = useState(false);
+  let [재고, 재고변경] = useState([10, 11, 12]);
 
 
 
@@ -32,8 +41,8 @@ function App() {
             - 일단 <Navbar>안의 버튼에 href 지우고
             - <Link to="경로">버튼</Link>
             React 문법 Link 경로 입력하면 이동 됨 */}
-            <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
-            <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -66,20 +75,29 @@ function App() {
           </Jumbotron>
           {/* container : 좌우 여백을 이쁘게 잡아줌 */}
           <div className="container">
-            <div className="row">
-              {
-                shoes.map((a, i) => {
-                  return (
-                    <Card shoes={shoes[i]} i={i}></Card>
-                  )
-                })
-              }
+            {/* value={공유하고싶은데이터} */}
+            <재고context.Provider value={재고}>
 
-              {/* <Card shoes={shoes[0]}></Card>
+              <div className="row">
+                {
+                  loding == true
+                    ? <Loding></Loding>
+                    : null
+                }
+
+                {
+                  shoes.map((a, i) => {
+                    return (
+                      <Card shoes={shoes[i]} i={i} key={i}></Card>
+                    )
+                  })
+                }
+
+                {/* <Card shoes={shoes[0]}></Card>
           <Card shoes={shoes[1]}></Card>
           <Card shoes={shoes[2]}></Card> */}
 
-              {/* <div className="col-md-4">
+                {/* <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg"
               width="100%" />
             <h4>{ shoes[0].title }</h4>
@@ -97,17 +115,39 @@ function App() {
             <h4>상품명</h4>
             <p>상품설명 & 가격</p>
           </div> */}
-            </div>
+              </div>
+            </재고context.Provider>
+            <button className="btn btn-primary" onClick={() => {
 
+              // POST 요청 방식
+              // axios.post('서버URL', { id : 'codingapple', pw : 1234 });
+
+              // 로딩중이라는 UI 띄움
+              loding변경(true);
+              // 성공하면 .then()
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+                .then((result) => {
+                  // 서버에서 데이터를 받아오면
+                  // 로딩중이라는 UI 안보이게 처리
+                  loding변경(false);
+                  shoes변경([...shoes, ...result.data]); //...연산자는 괄호를 벗겨줍니다.
+                  //shoes변경([{},{},{}, {},{},{}]) ... 사용하여 괄호를 벗겨 이런 모양이됌
+                })
+                .catch(() => {
+                  // 로딩중이라는 UI 안보이게 처리
+                  console.log("실패했어요.");
+                })
+
+            }}>더보기</button>
           </div>
         </Route>
-        
+
         {/* /detail/:id
         아무문자나 받겠다는 URL 작명법
         1. 콜론 뒤에 맘대로 작성
         2. 여러개 사용가능 */}
         <Route path="/detail/:id">
-          <Detail shoes={ shoes } />
+          <Detail shoes={shoes} 재고={재고} 재고변경={재고변경} />
         </Route>
 
         <Route path="/:id">
@@ -121,6 +161,11 @@ function App() {
 
 
 function Card(props) {
+
+  // useContext 훅으로 공유된 값 사용하기
+  // useContext(범위이름)로 공유된 값 사용하기
+  // 간단한 데이터 전송은 props 사용하기
+  let 재고 = useContext(재고context);
   console.log("props : ", props);
   return (
     <div className="col-md-4">
@@ -130,9 +175,26 @@ function Card(props) {
         width="100%" />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.content} & {props.shoes.price}</p>
+      {/* props 대신 context를 쓰자
+      - 하위 컴포넌트들이 props 없이도 부모의 값을 사용가능 */}
+      <Test></Test>
+
     </div>
   )
 
+}
+
+function Loding(props) {
+  return (
+    <h4>로딩중</h4>
+  )
+}
+
+function Test() {
+  let 재고 = useContext(재고context)
+  return (
+    <p> {재고[0]} </p>
+  )
 }
 
 // function Item(props) {
--- src/Detail.js
+++ src/Detail.js
@@ -1,4 +1,7 @@
-import { useHistory,useParams } from "react-router";
+/* eslint-disable */
+
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router";
 // yarn add styled-components
 import styled from 'styled-components';
 import './Detail.scss';
@@ -18,50 +21,129 @@ let 박스 = styled.div`
 let 제목 = styled.h4`
     font-size : 25px;
     // 색깔만 다른 제목이 여러개 필요하다면?
-    color : ${ props => props.색상 }
+    color : ${props => props.색상}
 `;
+
+// Hook으로 컴포넌트의 인생 중간중간에 뭔가 명령을 줄수 있음 ✔
+// e.g) <Detail> 퇴장 전에 이것좀 해주세요.
+
+// 이전 문법
+// Lifecycle Hook 몇개 (원래는 class 컴포넌트만 사용가능)
+// class Detail2 extends React.Component {
+
+//   // Detail2 컴포넌트가 Mount되었을 때 실행할 코드 ~
+//   // 랜더링이 됐을때 실행 됌
+//   componentDidMount() {
+//     //Ajax 같은 것도 이런 곳에 자주 사용
+//   }
+
+//   // Detail2 컴포넌트가 unmount되기 직전에 실행될 코드
+//   componentWillUnmount() {
+
+//   }
+// }
+
 
 function Detail(props) {
 
-    // useParams 사용해서 파라미더 전달 가능
-    let { id } = useParams();
-    let history = useHistory();
-    // let 찾은상품 = props.shoes.find(function(상품) {
-    //     return 상품.id == id
-    // })
-    let 찾은상품 = props.shoes.find(x => x.id == id);
+  // useEffect 훅 1
+  // 컴포넌트가 mount 되었을 때 (렌더링이 되었을 때 = 보일 때)
+  // 컴포넌트가 update 될 때
 
-    console.log("찾은상품 : ", 찾은상품);
+  // useEffect 훅 2
+  // 컴포넌트가 사라질 때 코드를 실행시킬 수도 있음
+  // return () => { 실행할 코드 ~~}  // unmount가 될때 실행 됌
 
-    return (
-      <div className="container">
-          <박스>
-            {/* [props 문법]
+  // useEffect 훅 3
+  // 여러개를 사용하고 싶다면
+
+  // UI 만드는법
+  // UI 보이고 / 안보이고 상태를 state로 저장
+  let [alert, alert변경] = useState(true);
+
+  let [inputData, inputData변경] = useState('');
+
+  useEffect(() => {
+    //2초 후에 저거 alert 창을 안보이게 해주셈
+    let 타이머 = setTimeout(() => { alert변경(false) }, 2000);
+
+    // setTimeout 이런거 쓸때 주의점
+    // 타이머 해제 스킬
+    // 오류 난 가능성이 많으니 return 을 사용하면 useEffect 종료 시
+    // 타이머를 지운다.
+    return () => { clearTimeout(타이머) }
+  }, [alert]); // 실행조건을 넣을 수 있다.
+  // alert를 넣을 시 alert의 state가 변경이 될때만 실행됌
+  // [] 빈칸일시 _가 변경이 될때만 useEffect 실행되라
+  // ✔ 팁 : [] 빈칸을 놔두게 되면 <Detail> 등장시 한 번 실행하고 끝남
+
+
+  // 여러개 일때 차례대로 실행이 된다
+  useEffect(() => {
+
+  });
+
+  // useParams 사용해서 파라미더 전달 가능
+  let { id } = useParams();
+  let history = useHistory();
+  // let 찾은상품 = props.shoes.find(function(상품) {
+  //     return 상품.id == id
+  // })
+  let 찾은상품 = props.shoes.find(x => x.id == id);
+
+  console.log("찾은상품 : ", 찾은상품);
+
+  return (
+    <div className="container">
+      <박스>
+        {/* [props 문법]
             보낼이름 = { 변수명 }
             보낼이름 = "일반문자" */}
-            <제목 className="red">Detail</제목>
-          </박스>
-            <div className="my-alert2">
-                <p>재고가 얼마 남지 않았습니다.</p>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-              </div>
-              <div className="col-md-6 mt-4">
-                <h4 className="pt-5">{찾은상품.title}</h4>
-                <p>{찾은상품.content}</p>
-                <p>{찾은상품.price}원</p>
-                <button className="btn btn-danger">주문하기</button>
-                <button className="btn btn-danger" onClick={() => {
-                    history.goBack();
-                    // history.push('/') : 특정 경로로 이동 시킬 때
-                }}>뒤로가기</button>
-              </div>
-            </div>
+        <제목 className="red">Detail</제목>
+      </박스>
+      {inputData}
+      {/* 입력 할 떄마다 재랜더링이 일어남
+          위의 useEffect도 계속 실행되는 문제 ?! 
+          useEffect에 []에 조건을 넣으면 useEffect가 실행되지 않음*/}
+      <input onChange={(e) => { inputData변경(e.target.value) }} />
+      {
+        alert == true
+          ? <div className="my-alert2">
+            <p>재고가 얼마 남지 않았습니다.</p>
           </div>
-    )
-  }
+          : null
+      }
+
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6 mt-4">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
+          <Info 재고={props.재고} />
+          <button className="btn btn-danger" onClick={() => {
+            // 하위 컴포넌트가 상위 컴포넌트 state변경하려면 state 변경함수 씁니다.
+            // 그게 상위 컴포넌트에 있으면 변경함수도 props로 전송해서 쓰셈
+            props.재고변경([9, 11, 12])   
+          }}>주문하기</button>
+          &nbsp;
+          <button className="btn btn-danger" onClick={() => {
+            history.goBack();
+            // history.push('/') : 특정 경로로 이동 시킬 때
+          }}>뒤로가기</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Info(props) {
+  return (
+    <p>재고 : { props.재고[0] }</p>
+  )
+}
 
 
 export default Detail;
